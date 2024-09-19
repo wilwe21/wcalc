c5a5328d90d36b73d6550f92e39ab119aff7f94d
wilwe <wilwe036@gmail.com>
2024-09-19 19:23:24 +0200

    compression

--- bwt/src/main.rs
+++ bwt/src/main.rs
@@ -10,6 +10,39 @@ fn movein(st: &str) -> String {
     compleat
 }
 
+fn compress(st: &str) -> String {
+    let s: Vec<_> = st.chars().collect();
+    let mut vec: Vec<Vec<String>> = Vec::new();
+    let mut count = 0;
+    let mut cur: char = '`'; 
+    for a in 0..s.len() {
+        if cur == '`' {
+            cur = s[a];
+            count += 1;
+        } else if cur == s[a] {
+            count += 1;
+        } else {
+            if count == 1 {
+                vec.push(vec![cur.to_string()]);
+            } else {
+                vec.push(vec![count.to_string(), cur.to_string()]);
+            }
+            cur = s[a];
+            count = 1;
+        }
+    }
+    if count == 1 {
+        vec.push(vec![cur.to_string()]);
+    } else {
+        vec.push(vec![count.to_string(), cur.to_string()]);
+    }
+    let mut end: Vec<String> = Vec::new();
+    for i in vec {
+        end.push(i.iter().map(|c| c.to_string()).collect::<Vec<String>>().join("").clone());
+    }
+    end.join("").clone()
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
@@ -27,4 +60,6 @@ fn main() {
     }
     let fin = end.iter().map(|c| c.to_string()).collect::<Vec<String>>().join("").clone();
     println!("{}", fin);
+    let compressed = compress(&fin);
+    println!("{}", compressed);
 }
