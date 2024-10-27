aa5a833d5bba80da78e933fb30dcb2df31c183f4
wilwe <wilwe036@gmail.com>
2024-10-27 12:48:06 +0100

    anti spam fin

--- src/conf.rs
+++ src/conf.rs
@@ -56,6 +56,22 @@ config button = true"
     }
 }
 
+pub fn str_to_conf(config: String) -> HashMap<String, String> {
+    let mut conf = HashMap::new();
+    let settings: Vec<_> = config.split('\n').filter(|x| x.to_string().contains("=") && !(x.to_string().contains("//")))
+        .map(|x| {
+            let spl = x.split("=").collect::<Vec<_>>();
+            let xxx = vec![spl[0].to_string(),spl[1..].join("=").to_string()];
+            xxx.into_iter()
+                .map(|y| y.to_string().trim().to_owned()).collect::<Vec<_>>()
+        }
+        ).collect();
+    for i in settings {
+        conf.insert(i[0].clone(), i[1].clone());
+    }
+    return conf
+}
+
 pub fn get_conf() -> HashMap<String, String> {
     match config_dir() {
         Some(h) => {
@@ -63,19 +79,7 @@ pub fn get_conf() -> HashMap<String, String> {
             match File::open(&path) {
                 Ok(f) => {
                     let cont = fs::read_to_string(path).expect("config file");
-                    let mut conf = HashMap::new();
-                    let settings: Vec<_> = cont.split('\n').filter(|x| x.to_string().contains("=") && !(x.to_string().contains("//")))
-                        .map(|x| {
-                            let spl = x.split("=").collect::<Vec<_>>();
-                            let xxx = vec![spl[0].to_string(),spl[1..].join("=").to_string()];
-                            xxx.into_iter()
-                                .map(|y| y.to_string().trim().to_owned()).collect::<Vec<_>>()
-                        }
-                        ).collect();
-                    for i in settings {
-                        conf.insert(i[0].clone(), i[1].clone());
-                    }
-                    return conf
+                    str_to_conf(cont.to_string())
                 },
                 _ => {
                     let s = init_conf();
