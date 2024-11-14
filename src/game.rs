93c526806eafa7ae30830d9f98a8019d44ad2183
wilwe <wilwe036@gmail.com>
2024-11-14 08:50:20 +0100

    map generating

--- src/game.rs
+++ src/game.rs
@@ -26,29 +26,80 @@ pub fn get_global_stats() -> HashMap<String, HashMap<String, String>> {
 
 pub fn new_conf() -> HashMap<String, HashMap<String, String>> {
     let mut d = include_str!("./stats.cfg").to_string();
-    d += &generate(6).to_string();
+    d += &"[map]\n".to_string();
+    d += &generate_map(6).to_string();
     save::str_to_conf(d.to_string())
 }
 
-pub fn generate(size: u8) -> String {
-    let leg = include_str!("./legend");
-    "#####\n#xxx#\n#####".to_string()
+pub fn generate_map(size: usize) -> String {
+    let mut ran = rand::thread_rng();
+    let mut map = String::new();
+    for i in 0..(size+1) {
+        map += &format!("{:#^size$}\n","#").to_string();
+    }
+    let mut map2 = map.split("\n").filter(|&x| x != "")
+        .map(|x| x.split("").filter(|&xx| xx != "").map(|xxx| xxx.to_string()).collect::<Vec<String>>())
+        .collect::<Vec<Vec<String>>>();
+    map2[0][0] = "0".to_string();
+    let mut cur = vec![0,0];
+    if size-1 <= 2 {
+        return map2.into_iter().map(|x| x.join("")).collect::<Vec<String>>().join("\n")
+    } else {
+        for i in 1..10 {
+            let num = ran.gen_range(0..2);
+            if num == 0 && !(cur[0]+1 > size-1) {
+                if cur[1] != 0 && cur[0] != 0 {
+                    let ar = ran.gen_range(0..2);
+                    if ar == 0 {
+                        if map2[cur[0]-1][cur[1]-1] == "#" && map2[cur[0]][cur[1]-1] == "#" {
+                            map2[cur[0]][cur[1]-1] = "a".to_string();
+                        } 
+                    } else if ar == 1 {
+                        if map2[cur[0]-1][cur[1]] == "#" && map2[cur[0]-1][cur[1]-1] == "#" {
+                            map2[cur[0]-1][cur[1]] = "a".to_string();
+                        } 
+                    }
+                }
+                cur[0] += 1;
+                map2[cur[0]][cur[1]] = format!("{}",i);
+            } else if !(cur[1]+1 > size-1)  {
+                if cur[1] != 0 && cur[0] != 0 {
+                    let ar = ran.gen_range(0..3);
+                    if ar == 0 {
+                        if map2[cur[0]][cur[1]-1] == "#" {
+                            map2[cur[0]][cur[1]-1] = "a".to_string();
+                        } 
+                    } else if ar == 1 {
+                        if map2[cur[0]-1][cur[1]] == "#" {
+                            map2[cur[0]-1][cur[1]] = "a".to_string();
+                        } 
+                    }
+                }
+                cur[1] += 1;
+                map2[cur[0]][cur[1]] = format!("{}",i);
+            }
+        }
+    }
+    return map2.into_iter().map(|x| x.join("")).collect::<Vec<String>>().join("\n")
+}
+
+pub fn generate_room(size: usize) -> String {
+    "###".to_string()
 }
 
 pub fn start() -> String {
     let mut conf = conf::get_conf();
     conf.insert("game".to_string(),"rpg".to_string());
     conf::save_conf(conf);
-    let mut d = include_str!("./stats.cfg").to_string();
-    d += &generate(6).to_string();
-    save::save(save::str_to_conf(d.to_string()));
-    "Game Started".to_string()
+    init_global_stats();
+    end()
 }
 
 pub fn end() -> String {
     let mut conf = conf::get_conf();
     conf.insert("game".to_string(),false.to_string());
     conf::save_conf(conf);
+    save::save(get_global_stats());
     "Game Ended".to_string()
 }
 
