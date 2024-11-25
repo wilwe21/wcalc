ed6ee1a97b7814cd563cff3e497aed597c7e80ce
wilwe <wilwe036@gmail.com>
2024-11-25 16:54:51 +0100

    move

--- src/game.rs
+++ src/game.rs
@@ -220,26 +220,57 @@ pub fn end_silent() {
 }
 
 pub fn rpginp(text: String, button: String) -> String {
-    let s = get_global_stats();
-    let player = s.get("player").unwrap();
+    let mut s = get_global_stats();
+    let mut player = s.get("player").unwrap().clone();
+    let roomid = player.get("room").unwrap().clone();
+    let room = s.get(&format!("room{}",roomid).to_string()).unwrap().clone();
     let mut pos: Vec<u8> = player.get("position").unwrap().split("x").map(|r| r.parse::<u8>().unwrap()).collect();
+    let mx: u8 = (room.get(&"0".to_string()).unwrap().len() -1).try_into().unwrap();
+    let my: u8 = (room.len()-1).try_into().unwrap();
+    let mut sus = HashMap::new();
+    sus.insert("roomcur".to_string(), room);
+    println!("{}", save::save(sus));
     println!("{:?}", pos);
     if button == "8" || text.ends_with("8") {
-        pos[1] += 1;
-    println!("{:?}", pos);
-        return "poruszasz się do przodu".to_string()
+        if pos[1] > 0 {
+            pos[1] -= 1;
+            player.insert("position".to_string(), format!("{}x{}", pos[0], pos[1]).to_string());
+            s.insert("player".to_string(), player);
+            init_global_stats(s);
+            return "up".to_string()
+        } else {
+            return "can't move".to_string()
+        }
     } else if button == "2" || text.ends_with("2") {
-        pos[1] -= 1;
-    println!("{:?}", pos);
-        return "poruszasz się do tyłu".to_string()
+        if pos[1] < mx {
+            pos[1] += 1;
+            player.insert("position".to_string(), format!("{}x{}", pos[0], pos[1]).to_string());
+            s.insert("player".to_string(), player);
+            init_global_stats(s);
+            return "down".to_string()
+        } else {
+            return "can't move".to_string();
+        }
     } else if button == "4" || text.ends_with("4") {
-        pos[0] -= 1;
-    println!("{:?}", pos);
-        return "poruszasz się w lewo".to_string()
+        if pos[0] > 0 {
+            pos[0] -= 1;
+            player.insert("position".to_string(), format!("{}x{}", pos[0], pos[1]).to_string());
+            s.insert("player".to_string(), player);
+            init_global_stats(s);
+            return "left".to_string()
+        } else {
+            return "can't move".to_string()
+        }
     } else if button == "6" || text.ends_with("6") {
-        pos[0] += 1;
-    println!("{:?}", pos);
-        return "poruszasz się w prawo".to_string()
+        if pos[0] < my {
+            pos[0] += 1;
+            player.insert("position".to_string(), format!("{}x{}", pos[0], pos[1]).to_string());
+            s.insert("player".to_string(), player);
+            init_global_stats(s);
+            return "right".to_string()
+        } else {
+            return "can't move".to_string()
+        }
     } else if button == "√" || text.ends_with("map") {
         return "otwierasz mape".to_string()
     }
