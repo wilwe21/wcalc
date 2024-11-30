8451df6574a7f94f6d94cddd6a3bab30486de9b4
wilwe <wilwe036@gmail.com>
2024-11-30 09:28:28 +0100

    backending

--- src/fui.rs
+++ src/fui.rs
@@ -31,7 +31,7 @@ pub fn get_fw() -> gtk::Window {
                     .title("Wcalc Fight")
                     .resizable(false)
                     .build();
-                win.set_default_size(200, 200);
+                win.set_default_size(400, 400);
                 set_fw(None);
                 return win
             }
@@ -42,10 +42,68 @@ pub fn get_fw() -> gtk::Window {
 pub fn update(pl: Entity, en: Entity) {
     let win = get_fw();
     let b = gtk::Box::new(gtk::Orientation::Vertical, 1);
-    let play = gtk::Label::builder().label(format!("{}: {}hp", pl.character, pl.health)).build();
-    let enemigo = gtk::Label::builder().label(format!("{}: {}hp", en.character, en.health)).build();
-    b.append(&play);
-    b.append(&enemigo);
+    let b1 = gtk::Box::new(gtk::Orientation::Horizontal, 1);
+    let b11 = gtk::Box::new(gtk::Orientation::Vertical, 1);
+    let b12 = gtk::Box::new(gtk::Orientation::Vertical, 1);
+    let b2 = gtk::Box::new(gtk::Orientation::Horizontal, 1);
+    let b21 = gtk::Box::new(gtk::Orientation::Vertical, 1);
+    let b22 = gtk::Box::new(gtk::Orientation::Vertical, 1);
+    let b3 = gtk::Box::new(gtk::Orientation::Horizontal, 1);
+    let b31 = gtk::Box::new(gtk::Orientation::Vertical, 1);
+    let b32 = gtk::Box::new(gtk::Orientation::Vertical, 1);
+    b1.add_css_class("box1");
+    b2.add_css_class("box2");
+    b3.add_css_class("box3");
+    b11.add_css_class("box11");
+    b21.add_css_class("box21");
+    b31.add_css_class("box31");
+    b11.set_halign(gtk::Align::Start);
+    b21.set_halign(gtk::Align::Start);
+    b31.set_halign(gtk::Align::Start);
+    b12.set_halign(gtk::Align::End);
+    b22.set_halign(gtk::Align::End);
+    b32.set_halign(gtk::Align::End);
+    b21.set_valign(gtk::Align::End);
+    b22.set_valign(gtk::Align::End);
+    b12.add_css_class("box12");
+    b22.add_css_class("box22");
+    b32.add_css_class("box32");
+    b.add_css_class("fightbox");
+    b.set_hexpand(true);
+    b1.set_vexpand(true);
+    b2.set_vexpand(true);
+    b3.set_vexpand(true);
+    b11.set_hexpand(true);
+    b21.set_hexpand(true);
+    b31.set_hexpand(true);
+    b12.set_hexpand(true);
+    b22.set_hexpand(true);
+    b32.set_hexpand(true);
+    let plbox = gtk::Box::new(gtk::Orientation::Vertical, 1);
+    let plname = gtk::Label::builder().label(format!("{}", pl.character)).build();
+    let plhp = gtk::Label::builder().label(format!("{}hp", pl.health)).build();
+    plbox.add_css_class("playerbox");
+    plbox.append(&plname);
+    plbox.append(&plhp);
+    let enbox = gtk::Box::new(gtk::Orientation::Vertical, 1);
+    let enemigoname = gtk::Label::builder().label(format!("{}", en.character)).build();
+    let enemigohp = gtk::Label::builder().label(format!("{}hp", en.health)).build();
+    enbox.set_width_request(150);
+    plbox.set_width_request(150);
+    enbox.add_css_class("enemybox");
+    enbox.append(&enemigoname);
+    enbox.append(&enemigohp);
+    b.append(&b1);
+    b.append(&b2);
+    b.append(&b3);
+    b1.append(&b11);
+    b1.append(&b12);
+    b2.append(&b21);
+    b2.append(&b22);
+    b3.append(&b31);
+    b3.append(&b32);
+    b11.append(&enbox);
+    b22.append(&plbox);
     win.set_child(Some(&b));
     set_fw(Some(win));
 }
